9d973e434dfb507d93962d92ec3fccff7161930b
Shaun Tan <shauntanzongzhi@gmail.com>
2022-04-08 19:45:02 +0800

    Add javadoc check to 'run_q1_tests.sh'

--- run_q1_tests.sh
+++ run_q1_tests.sh
@@ -2,7 +2,7 @@ clear;
 echo -e "\n\n===============================================================\n\n"
 javac -Xlint:unchecked,rawtypes cs2030s/fp/*.java
 
-#javac Test1.java; javac Test2.java; javac Test3.java; javac Test4.java
+javac Test1.java; javac Test2.java; javac Test3.java; javac Test4.java
 
 java Test1 | grep failed
 java Test2 | grep failed
@@ -10,3 +10,4 @@ java Test3 | grep failed
 java Test4 | grep failed
 
 java -jar ~cs2030s/bin/checkstyle.jar -c ~cs2030s/bin/cs2030_checks.xml cs2030s/fp/Try.java
+javadoc -quiet -private -d docs cs2030s/fp/Try.java
